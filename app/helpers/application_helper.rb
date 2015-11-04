098bbcd055c172ff14fe014f4dcca64227c26596
Brian Underwood <ml+github@semi-sentient.com>
2015-11-04 12:56:31 +0800

    Rubocop: Style/SpaceAroundEqualsInParameterDefault

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 
   # Copied from https://github.com/dongli/mathjax-rails
   # PR has been sitting there for a while...
-  def mathjax_tag(opt={})
+  def mathjax_tag(opt = {})
     opt[:config] ||= 'TeX-AMS_HTML-full.js'
     opt[:config] = nil if opt[:config] == false
     "<script src=\"#{main_app.mathjax_path(uri: 'MathJax.js', config: opt[:config])}\" type=\"text/javascript\"></script>".html_safe
