4e17d78e0d93696f891ed86e9217861fad7daa8d
Brian Underwood <ml+github@semi-sentient.com>
2015-11-19 16:33:54 +0530

    Add change from previous graph gist portal to standardize Neo4j versions

--- app/assets/javascripts/graphgist.js
+++ app/assets/javascripts/graphgist.js
@@ -44,15 +44,6 @@ function GraphGist($, options) {
     var DEFAULT_VERSION = '2.3';
     var CONSOLE_VERSIONS = { '2.0.0-M06': 'http://neo4j-console-20m06.herokuapp.com/',
         '2.0.0-RC1': 'http://neo4j-console-20rc1.herokuapp.com/',
-        '2.0.0': 'http://neo4j-console-20.herokuapp.com/',
-        '2.0.1': 'http://neo4j-console-20.herokuapp.com/',
-        '2.0.2': 'http://neo4j-console-20.herokuapp.com/',
-        '2.0.3': 'http://neo4j-console-20.herokuapp.com/',
-        '2.0.4': 'http://neo4j-console-20.herokuapp.com/',
-        '2.1.0': 'http://neo4j-console-21.herokuapp.com/',
-        '2.1.1': 'http://neo4j-console-21.herokuapp.com/',
-        '2.1.2': 'http://neo4j-console-21.herokuapp.com/',
-        '2.1.3': 'http://neo4j-console-21.herokuapp.com/',
         '2.1': 'http://neo4j-console-21.herokuapp.com/',
         '2.2': 'http://neo4j-console-22.herokuapp.com/',
         '2.3': 'http://neo4j-console-23.herokuapp.com/',
@@ -149,6 +140,10 @@ function GraphGist($, options) {
     function postProcessPage() {
         var $meta = $('#metadata', $content);
         var version = $meta.attr('version'), tags = $meta.attr('tags'), author = $meta.attr('author'), twitter = $meta.attr('twitter');
+        regex = /^(\d+)\.(\d+)\.\d+$/;
+        if (version.match(regex)) {
+          version = version.replace(regex, '$1.$2');
+        }
         if (tags === '{tags}') {
             tags = false;
         }
