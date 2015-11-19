e5f07c12a913454e1f17870de7742cc23de8c969
Brian Underwood <ml+github@semi-sentient.com>
2015-11-19 12:19:29 +0530

    JSON API for featured GraphGists

---
+++ app/views/info/featured_graphgists.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @featured_graphgists
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mathjax 'mathjax'
 
   root 'info#featured_graphgists'
+  get 'featured_graphgists(.:format)' => 'info#featured_graphgists'
   get 'about' => 'info#about'
   get 'submit_graphgist' => 'info#submit_graphgist'
   get 'preview_graphgist' => 'info#preview_graphgist'
