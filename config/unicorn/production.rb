75aa9d75c1ab919edf1efd8211f5f7c0c58eab4d
Brian Underwood <ml+github@semi-sentient.com>
2015-11-27 09:46:06 +0530

    Add unicorn production config

---
+++ config/unicorn/production.rb
@@ -0,0 +1,19 @@
+app_dir = File.expand_path("../../..", __FILE__)
+working_directory app_dir
+
+
+# Set unicorn options
+worker_processes 3
+preload_app true
+timeout 30
+
+# Set up socket location
+listen "#{app_dir}/tmp/sockets/unicorn.sock", :backlog => 64
+
+# Logging
+stderr_path "#{app_dir}/log/unicorn.stderr.log"
+stdout_path "#{app_dir}/log/unicorn.stdout.log"
+
+# Set master PID location
+pid "#{app_dir}/tmp/pids/unicorn.pid"
+
