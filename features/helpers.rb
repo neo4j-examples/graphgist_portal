d49e051c004b171660020e63dfc60cf4859e86c9
Brian Underwood <ml+github@semi-sentient.com>
2015-12-06 18:12:41 +0530

    Rubocop

--- features/helpers.rb
+++ features/helpers.rb
@@ -5,16 +5,15 @@ When(/^The URL ([^ ]+) is visited$/) do |url|
 end
 
 When(/^The path ([^ ]+) is visited$/) do |url|
-  @response = Faraday.get($host + url)
+  @response = Faraday.get(RailsGraphgistPortal.host + url)
 end
 
 
 Then(/^A redirect is given to ([^ ]+)$/) do |url|
-  expect(@response.status).should == 301
-  expect(@response.headers['location']).should == url
+  expect(@response.status).to eq 301
+  expect(@response.headers['location']).to eq url
 end
 
 Then(/^A page with "([^"]*)" is returned$/) do |text|
   expect(@response.body).to include(text)
 end
-
--- features/rails_graphgist_portal/helpers.rb
+++ features/rails_graphgist_portal/helpers.rb
@@ -1,1 +1,8 @@
-$host = 'http://graphgist-portal.herokuapp.com'
+# Context variables for testing the Rails GraphGist portal
+module RailsGraphgistPortal
+  class << self
+    attr_accessor :host
+  end
+end
+
+RailsGraphgistPortal.host = 'http://graphgist-portal.herokuapp.com'
