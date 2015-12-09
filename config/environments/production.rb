3d4049534961e1e78140fe950c589fabf7ccf906
Brian Underwood <ml+github@semi-sentient.com>
2015-12-09 12:18:10 +0700

    Update hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'http://graphgist-portal.herokuapp.com/'
+  config.action_controller.asset_host = 'http://portal.graphgist.org/'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -74,7 +74,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = {host: 'graphgist-portal.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'portal.graphgist.org'}
 
   config.neo4j.session_type = :server_db
   config.neo4j.session_path = ENV['NEO4J_URL']
