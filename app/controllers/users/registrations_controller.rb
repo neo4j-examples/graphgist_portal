010f30a1eb2a13b2e1f49e623679bc51b6dc482f
Brian Underwood <ml+github@semi-sentient.com>
2015-12-11 15:47:34 +0700

    Allow setting of details like t-shirt size

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,11 +6,13 @@ module Users
     protected
 
     def update_resource(resource, params)
-      resource.update_without_password(params)
+      resource.person.update_attributes(params[:person]) if resource.person
+      resource.update_without_password(params.except(:person))
     end
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up).push(:name, :email, :twitter_username)
+      devise_parameter_sanitizer.for(:account_update).push(:name, :email, person: [:tshirt_size, :tshirt_size_other])
     end
   end
 end
