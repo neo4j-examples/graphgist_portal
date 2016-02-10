9cc35f16c4d48471e860c0c875af5ad380af79bd
Brian Underwood <ml+github@semi-sentient.com>
2016-02-10 13:37:32 +0700

    Allow style tag in gists

--- app/models/graph_gist.rb
+++ app/models/graph_gist.rb
@@ -71,7 +71,7 @@ class GraphGist < GraphStarter::Asset
   end
 
   SANITIZER = Rails::Html::WhiteListSanitizer.new
-  VALID_HTML_TAGS = %w(a b body code col colgroup div em h1 h2 h3 h4 h5 h6 hr html i img li ol p pre span strong table tbody td th thead tr ul)
+  VALID_HTML_TAGS = %w(a b body code col colgroup div em h1 h2 h3 h4 h5 h6 hr html i img li ol p pre span strong style table tbody td th thead tr ul)
   VALID_HTML_ATTRIBUTES = %w(id src href class style data-style graph-mode)
 
   def place_asciidoc(asciidoc_text)
