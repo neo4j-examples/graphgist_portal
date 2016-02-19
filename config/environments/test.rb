9de9ab103725971daa25c6df1d67d03320377782
Brian Underwood <ml+github@semi-sentient.com>
2016-02-19 13:02:46 +0700

    Moving URL to gitignored yml config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,7 +39,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  config.neo4j.session_type = :server_db
-  config.neo4j.session_path = 'http://localhost:6666'
 end
