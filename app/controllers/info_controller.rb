f5090dba8d911a59d5febc9974168e2a16e75da3
ryguyrg <ryan@ryugy.com>
2016-03-03 17:57:13 -0800

    try restricting by category

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -8,7 +8,7 @@ class InfoController < ApplicationController
 
   def live_graphgists
     var = :asset
-    @live_graphgists = apply_associations(model_class.all(var).for_category(var, params[:category]).where(status: 'live')).to_a
+    @live_graphgists = apply_associations(for_category(GraphGists.only_live, params[:category]).where(status: 'live')).to_a
   end
 
   CONSOLE_HOSTS = {
