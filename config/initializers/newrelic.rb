b68ff87fa73784b1f87ee0ebb13695329c32ff25
Brian Underwood <ml+github@semi-sentient.com>
2016-03-10 21:33:05 +0800

    Use the ActiveSupport instrumentation instead

--- config/initializers/newrelic.rb
+++ config/initializers/newrelic.rb
@@ -1,12 +1,8 @@
 if defined?(NewRelic)
   require 'new_relic/agent/datastores'
 
-  module Neo4j
-    module Server
-      # Re-opening class to add newrelic monitoring
-      class CypherSession < Neo4j::Session
-        NewRelic::Agent::Datastores.trace self, :_query, 'Neo4j'
-      end
-    end
+  ActiveSupport::Notifications.subscribe('neo4j.cypher_query') do |_, start, finish, _id, payload|
+    NewRelic::Agent::Datastores.notice_statement(payload[:cypher], finish - start)
   end
 end
+
