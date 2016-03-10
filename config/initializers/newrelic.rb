36093076d845f7f0c7eeaea9dfb4fde2195f7612
Brian Underwood <ml+github@semi-sentient.com>
2016-03-10 21:12:13 +0800

    Only when NewRelic is defined (current in production)

--- config/initializers/newrelic.rb
+++ config/initializers/newrelic.rb
@@ -1,10 +1,12 @@
-require 'new_relic/agent/datastores'
+if defined?(NewRelic)
+  require 'new_relic/agent/datastores'
 
-module Neo4j
-  module Server
-    # Re-opening class to add newrelic monitoring
-    class CypherSession < Neo4j::Session
-      NewRelic::Agent::Datastores.trace self, :_query, 'Neo4j'
+  module Neo4j
+    module Server
+      # Re-opening class to add newrelic monitoring
+      class CypherSession < Neo4j::Session
+        NewRelic::Agent::Datastores.trace self, :_query, 'Neo4j'
+      end
     end
   end
 end
