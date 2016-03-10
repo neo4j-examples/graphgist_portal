c74c2ad72805d107d4cfbc1c0f3c75946b8e88d0
Brian Underwood <ml+github@semi-sentient.com>
2016-03-10 21:07:54 +0800

    Rubocop

--- config/initializers/newrelic.rb
+++ config/initializers/newrelic.rb
@@ -2,9 +2,9 @@ require 'new_relic/agent/datastores'
 
 module Neo4j
   module Server
+    # Re-opening class to add newrelic monitoring
     class CypherSession < Neo4j::Session
-      NewRelic::Agent::Datastores.trace self, :_query, "Neo4j"
+      NewRelic::Agent::Datastores.trace self, :_query, 'Neo4j'
     end
   end
 end
-
