719a25deef5bfb3664cec0e148cdeaa7da2fc481
Brian Underwood <ml+github@semi-sentient.com>
2016-03-17 10:41:49 +0800

    From address should be graphgist.org so as not to be quarantined.  Reply-to to devrel@neo4j.com

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,4 @@
 # Base for all mailers in the application
 class ApplicationMailer < ActionMailer::Base
-  default from: 'graphgist_portal@neo4j.com' # DOES NOT YET EXIST
-  #  layout 'mailer'
+  default from: 'graphgist_portal@graphgist.org', reply_to: 'devrel@neo4j.com'
 end
