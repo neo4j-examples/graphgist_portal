240b67fee125620ea812e926a7cc9c131c56014a
Brian Underwood <ml+github@semi-sentient.com>
2016-04-05 15:11:06 +0900

    Rubocop

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -10,7 +10,7 @@ class QueryController < ApplicationController
 
   before_action :access_control_allow_all
 
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def graph_gist_query_session_id
     session_id = SecureRandom.uuid
