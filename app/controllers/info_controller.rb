0b0fdec73d09ab0f8290212a3eff27379588844b
Alisson Patricio <eu@alisson.net>
2018-09-03 19:46:28 -0300

    GGP-190: add neo4j-version cache field
    this is so we can easily retrieve this information without parsing the asciidoc

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -56,6 +56,7 @@ class InfoController < ApplicationController
         '.updated_at,'\
         '.featured,'\
         '.created_at,'\
+        '.neo4j_version,'\
         'author: author {.uuid, .name, .slug},'\
         'image: head(collect(image)),'\
         'industries: collect(distinct industry {.uuid, .name, .slug, image: industry_image}),'\
@@ -70,6 +71,7 @@ class InfoController < ApplicationController
         title: asset[:title],
         name: asset[:title],
         summary: asset[:summary],
+        neo4j_version: asset[:neo4j_version],
         id: asset[:uuid],
         slug: asset[:slug],
         model_slug: params[:model_slug],
--- lib/tasks/graphgist.rake
+++ lib/tasks/graphgist.rake
@@ -9,4 +9,14 @@ namespace :graphgist do
     end
   end
 
+  desc "Populate neo4j_version field"
+  task populate_neo4j_version: :environment do
+    GraphGist.find_each do |g|
+      if !g.neo4j_version.present?
+        g.place_neo4j_version
+        g.save
+        puts "GraphGist: #{g.id} => #{g.neo4j_version}\n"
+      end
+    end
+  end
 end
